174ef7647d580e38aeb5061cf4f4df52c752e633
VicenteAparicio <miradadesigual@gmail.com>
2023-09-22 12:51:07 +0200

    Check validations and implement errors

--- src/components/InsertData/insertData.tsx
+++ src/components/InsertData/insertData.tsx
@@ -1,14 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { IDropdown } from "../../pages/models/IDropdown";
 import { Dropdown } from "../Dropdown/dropdown";
 import { IData } from "../../pages/models/IDataModel";
 import dataService from "../../services/data.service";
-import { NavLink } from "react-router-dom";
 import AuthService from "../../services/auth.service";
 
 const baseClass = "insertData";
 
 export const InsertData: React.FC = () => {
+
+    const [validate, setValidate] = useState<boolean>(false);
+    const [buttonClassValidation, setButtonClassValidation] = useState('')
+    const [errors, setErrors] = useState({ eAño: '', eTemperatura: '', eGraduacion: '', ePh: '', eObservaciones: '' });
     const [insertObject, setInsertObject] = useState<IData>({
         userId: 0,
         año: 0,
@@ -21,9 +24,57 @@ export const InsertData: React.FC = () => {
         observaciones: ''
     });
 
-    // const [errors, setErrors] = useState({ eAño: '', eVariedad: '', eTipo: '', eColor: '', eTemperatura: '', eGraduacion: '', ePh: '', eObservaciones: '' });
+    const ageRG = new RegExp(/^(\d{4})$/);
+    const tempRG = new RegExp(/^(\d{1}|\d{2})$/);
+    const textRG = new RegExp(/^(?!\s*$).+/g);
+
+    useEffect(() => {
+        if (errors.eAño === ''
+            && errors.eTemperatura === ''
+            && errors.eGraduacion === ''
+            && errors.ePh === '') {
+            setValidate(true);
+            setButtonClassValidation('');
+        } else {
+            setButtonClassValidation('buttonClassValidation');
+        }
+    }, [errors]);
 
     const handleInsertObject = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const arg = event.currentTarget.name;
+        const value = event.currentTarget.value;
+        switch (arg) {
+            case 'año':
+                if (!ageRG.test(value)) {
+                    setErrors({ ...errors, eAño: "Input can't be empty and should be a number of 4 digits." });
+                } else {
+                    setErrors({ ...errors, eAño: '' });
+                }
+                break
+            case 'temperatura':
+                if (!tempRG.test(value)) {
+                    setErrors({ ...errors, eTemperatura: "Input can't be empty and should be a number of 1 or 2 digits." });
+                } else {
+                    setErrors({ ...errors, eTemperatura: '' });
+                }
+                break
+            case 'graduacion':
+                if (!tempRG.test(value)) {
+                    setErrors({ ...errors, eGraduacion: "Input can't be empty and should be a number of 1 or 2 digits." });
+                } else {
+                    setErrors({ ...errors, eGraduacion: '' });
+                }
+                break
+            case 'ph':
+                if (!tempRG.test(value)) {
+                    setErrors({ ...errors, ePh: "Input can't be empty and should be a number of 1 or 2 digits." });
+                } else {
+                    setErrors({ ...errors, ePh: '' });
+                }
+                break
+            default:
+                break
+        }
         setInsertObject({
             ...insertObject,
             [event.currentTarget.name]: event.currentTarget.value
@@ -48,29 +99,25 @@ export const InsertData: React.FC = () => {
         })
     };
 
-
     const handleTextArea = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
         setInsertObject({
             ...insertObject,
             observaciones: event.currentTarget.value
         })
-        addData()
     }
 
     const addData = () => {
-        const currentUser = AuthService.getCurrentUser();
+        if (!validate) {
+            const currentUser = AuthService.getCurrentUser();
 
-        const body: IData = {
-            ...insertObject,
-            userId: currentUser?.data.id!
+            const body: IData = {
+                ...insertObject,
+                userId: currentUser?.data.id!
+            }
+            dataService.insert(body);
+
+            dataService.getAll();
         }
-
-        console.log("Esto : ", body)
-        const addDataResponse = dataService.insert(body);
-
-        // dataService.getAll();
-
-        // console.log(addDataResponse)
     }
 
     const variedades: IDropdown[] = [
@@ -97,8 +144,8 @@ export const InsertData: React.FC = () => {
         <div className={baseClass}>
             <form className="container">
                 <label className="commonLabel">Año</label>
-                <input className="commonInput" type="text" name='año' onChange={handleInsertObject} placeholder='Año' />
-                {/* <div className="validateError">{errors.eAño}</div> */}
+                <input required={true} className="commonInput" type="text" name='año' onChange={handleInsertObject} placeholder='Año' />
+                <div className="validateError">{errors.eAño}</div>
                 <label className="commonLabel">Variedad</label>
                 <Dropdown name='variedad' options={variedades} onSelect={handleVariedad} />
                 <label className="commonLabel">Tipo</label>
@@ -106,15 +153,15 @@ export const InsertData: React.FC = () => {
                 <label className="commonLabel">Color</label>
                 <Dropdown name='color' options={colores} onSelect={handleColor} />
                 <label className="commonLabel">Temperatura</label>
-                <input className="commonInput" type="text" name='temperatura' onChange={handleInsertObject} placeholder='Temperatura' />
-                {/* <div className="validateError">{errors.eTemperatura}</div> */}
-                <input className="commonInput" type="text" name='graduacion' onChange={handleInsertObject} placeholder='Graduacion' />
-                {/* <div className="validateError">{errors.eGraduacion}</div> */}
-                <input className="commonInput" type="text" name='ph' onChange={handleInsertObject} placeholder='Ph' />
-                {/* <div className="validateError">{errors.ePh}</div> */}
-                <textarea className="textAreaInput" name='observaciones' onChange={handleTextArea} placeholder='Observaciones' />
-                {/* <div className="validateError">{errors.ePh}</div> */}
-                <button className="buttonCard" onClick={() => addData()}>
+                <input required={true} className="commonInput" type="text" name='temperatura' onChange={handleInsertObject} placeholder='Temperatura' />
+                <div className="validateError">{errors.eTemperatura}</div>
+                <input required={true} className="commonInput" type="text" name='graduacion' onChange={handleInsertObject} placeholder='Graduacion' />
+                <div className="validateError">{errors.eGraduacion}</div>
+                <input required={true} className="commonInput" type="text" name='ph' onChange={handleInsertObject} placeholder='Ph' />
+                <div className="validateError">{errors.ePh}</div>
+                <textarea required={true} className="textAreaInput" name='observaciones' onChange={handleTextArea} placeholder='Observaciones' />
+                <div className="validateError">{errors.ePh}</div>
+                <button className={`button ${buttonClassValidation}`} onClick={() => addData()}>
                     Add
                 </button>
             </form>
